37fc36cff6d971a65302b198af9d577f6257d1e7
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-10-16 13:44:46 +0200

    Document API endpoints

--- src/server.js
+++ src/server.js
@@ -38,8 +38,79 @@ if (env.isProduction()) {
   });
 }
 
+/**
+ * AddressState fetch by address: without bidgroups, to sign with last id.
+ * @param  {string} address - User address
+ * @return {object}         - Address state object
+ */
+app.get(
+  "/api/addressState/simple/:address",
+  server.handleRequest(async (req, res) => {
+    // const param = server.extractFromReq(req, 'param')
+    return "success";
+  })
+);
+
+/**
+ * AddressState fetch by address: /full contains all BidGroups.
+ * @param  {string} address - User address
+ * @return {object}         - Address state object
+ */
+app.get(
+  "/api/addressState/full/:address",
+  server.handleRequest(async (req, res) => {
+    // const param = server.extractFromReq(req, 'param')
+    return "success";
+  })
+);
+
+/**
+ * ParcelState fetch by id. Attachs the bid history to the response
+ * @param  {string} id - ParcelState id
+ * @return {object}    - ParcelState object
+ */
+app.get(
+  "/api/parcelState/:id",
+  server.handleRequest(async (req, res) => {
+    // const param = server.extractFromReq(req, 'param')
+    return "success";
+  })
+);
+
+/**
+ * ParcelState group fetch. Get multiple parcel states at a time using an array
+ * @param ${array} coordinates - array of parcel state ids (represented by x,y coordinates). If you need a single ParcelState, use "/api/parcelState/:id" or an array of a single element
+ * @return {array}             - array of ParcelState objects
+ */
+app.get(
+  "/api/parcelState/group/:coordinates",
+  server.handleRequest(async (req, res) => {
+    // const param = server.extractFromReq(req, 'param')
+    return "success";
+  })
+);
+
+/**
+ * Parcel Range query, gets parcel states via max/min coordinates
+ * @param  {string} mincoords - String with the format "x,y", which represents the lower coordinate bound
+ * @param  {string} maxcoords - String with the format "x,y", which represents the upper coordinate bound
+ * @return {array}            - array of ParcelState objects
+ */
+app.get(
+  "/api/parcelState/range/:mincoords/:maxcoords",
+  server.handleRequest(async (req, res) => {
+    // const param = server.extractFromReq(req, 'param')
+    return "success";
+  })
+);
+
+/**
+ * Submit a BidGroup
+ * @param  {object} bidgroup - BidGroup attributes
+ * @return {boolean}         - Wether the operation was successfull or not
+ */
 app.post(
-  "/api/test",
+  "/api/bidgroup",
   server.handleRequest(async (req, res) => {
     // const param = server.extractFromReq(req, 'param')
     return "success";
