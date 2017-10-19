8d1666bc13f93a2648f6d257890c73435319504a
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-10-19 12:39:54 +0200

    Upsert the Roads of Decentraland as a project

--- scripts/init.js
+++ scripts/init.js
@@ -11,13 +11,59 @@ env.load();
 async function run() {
   eth.connect();
 
+  await upsertRoadsProject();
   await importAddressStates();
   await initParcels();
 }
 
+async function upsertRoadsProject() {
+  if (!await Project.findByName("Roads")) {
+    console.log("Inserting Roads project");
+
+    await Project.insert({
+      name: "Roads",
+      desc: "Decentraland roads connecting districts",
+      link: "",
+      public: false,
+      parcels: 0,
+      priority: 0,
+      disabled: false
+    });
+  }
+}
+
+async function initParcels() {
+  const parcels = fs.readFileSync("parcelsDescription.example.json", "utf8");
+  const { x, y, reserved, roads } = JSON.parse(parcels);
+
+  console.log(`Inserting a ${x.max}x${y.max} Matrix`);
+  new ParcelStateService(ParcelState).insertMatrix(x.max, y.max);
+
+  await reserveProjects(reserved);
+  await reserveProjects(roads);
+}
+
+async function reserveProjects(reservation) {
+  console.log("Reserving project parcels");
+
+  for (const projectName in reservation) {
+    const project = await Project.findByName(projectName);
+    if (!project) {
+      throw new Error(`Could not find project ${projectName}.`);
+    }
+
+    for (let coord of reservation[projectName]) {
+      console.log(
+        `Reserving parcel ${coord} for project ${projectName} ( ${project.id} )`
+      );
+      await ParcelState.update({ projectId: project.id }, { id: coord });
+    }
+  }
+}
+
 async function importAddressStates() {
   // - Read a dump of address => Balance
-  let addresses = fs.readFileSync("addresses.txt", "utf8");
+  let addresses = fs.readFileSync("addresses.example.txt", "utf8");
   addresses = addresses.split("\n").slice(10);
 
   for (let address of addresses) {
@@ -33,32 +79,6 @@ async function importAddressStates() {
       await AddressState.insert({ address, balance });
     }
   }
-}
-
-async function initParcels() {
-  const parcels = fs.readFileSync("parcelsDescription.json", "utf8");
-  const { x, y, reserved } = JSON.parse(parcels);
-
-  console.log(`Inserting a ${x.max}x${y.max} Matrix`);
-  new ParcelStateService(ParcelState).insertMatrix(x.max, y.max);
-
-  console.log("Reserving project parcels");
-  for (const projectName in reserved) {
-    const project = await Project.findByName(projectName);
-    if (!project) {
-      throw new Error(`Could not find project ${projectName}.`);
-    }
-
-    for (let coord of reserved[projectName]) {
-      console.log(
-        `Reserving parcel ${coord} for project ${projectName} ( ${project.id} )`
-      );
-      await ParcelState.update({ projectId: project.id }, { id: coord });
-    }
-  }
-
-  // TODO: Handle roads.
-  // We could add a roads project, for it could work the same way as projects do
 }
 
 db
