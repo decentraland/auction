0f61cff42f1ef3307567c24e58b80f63ffa28a68
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-11-14 17:12:50 +0100

    Add the `createSchema.js` script

---
+++ scripts/createSchema.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env babel-node
+
+import { env } from "decentraland-commons";
+import db from "../src/lib/db";
+
+env.load();
+
+db
+  .connect()
+  .catch(console.error)
+  .then(() => process.exit());
