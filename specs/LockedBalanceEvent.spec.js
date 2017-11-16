7cb10f97053b6d0990033d44aac6db693f962f62
Ariel Barmat <abarmat@gmail.com>
2017-11-16 18:34:11 -0300

    Add test for LockedBalanceEvent

---
+++ specs/LockedBalanceEvent.spec.js
@@ -0,0 +1,55 @@
+import { expect } from "chai";
+import { eth, utils } from "decentraland-commons";
+
+import db from "../src/lib/db";
+import { LockedBalanceEvent } from "../src/lib/models";
+
+describe("LockedBalanceEvent", () => {
+  const testEvents = [
+    {
+      address: "0xdead",
+      txId: "0x1",
+      mana: 1000,
+      confirmedAt: new Date("2017-10-01 12:00:00")
+    },
+    {
+      address: "0xdead",
+      txId: "0x2",
+      mana: 2000,
+      confirmedAt: new Date("2017-10-02 12:00:00")
+    },
+    {
+      address: "0xdead",
+      txId: "0x3",
+      mana: 3000,
+      confirmedAt: new Date("2017-11-01 12:00:00")
+    },
+    {
+      address: "0xlive",
+      txId: "0x4",
+      mana: 1000,
+      confirmedAt: new Date("2017-10-01 12:00:00")
+    },
+    {
+      address: "0xlive",
+      txId: "0x5",
+      mana: 2000,
+      confirmedAt: new Date("2017-11-01 12:00:00")
+    }
+  ];
+
+  beforeEach(async () => {
+    await Promise.all(testEvents.map(row => LockedBalanceEvent.insert(row)));
+  });
+
+  describe(".getMonthlyLockedBalanceByAddress", async () => {
+    it("should get locked balance of address by each month", async () => {
+      const balances = await LockedBalanceEvent.getMonthlyLockedBalanceByAddress(
+        "0xdead"
+      ).then(rows => utils.arrayToObject(rows, "month", "mana"));
+
+      expect(balances["10"]).to.be.equal("3000");
+      expect(balances["11"]).to.be.equal("3000");
+    });
+  });
+});
