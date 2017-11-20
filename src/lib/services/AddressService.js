97a5e4e8ff24a6d8516f42018616ddf8e31787cd
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-11-20 13:32:11 +0100

    Move things around in AddressService to follow the other services implementations.

--- src/lib/services/AddressService.js
+++ src/lib/services/AddressService.js
@@ -1,55 +1,25 @@
-import {
-  AddressState,
-  DistrictEntry,
-  LockedBalanceEvent,
-  ReturnTransaction
-} from "../models";
+import { DistrictEntry, LockedBalanceEvent } from "../models";
 
 const LAND_MANA_COST = 1000;
 const BEFORE_NOVEMBER_DISCOUNT = 1.15;
 const AFTER_NOVEMBER_DISCOUNT = 1.1;
 
-const fillByMonth = items => {
-  const months = new Array(12).fill(0);
-  const groups = months.reduce(
-    (obj, _, index) => Object.assign(obj, { [index + 1]: 0 }),
-    {}
-  );
-
-  for (let { month, mana } of items) {
-    groups[month] = parseInt(mana, 10);
-  }
-  return groups;
-};
-
-const calculateTotalForMonths = (
-  monthlyLockedBalancesToDistricts,
-  monthlyLockedBalancesTotal,
-  months
-) => {
-  return months.reduce((total, index) => {
-    return (
-      total +
-      monthlyLockedBalancesTotal[index] -
-      monthlyLockedBalancesToDistricts[index]
-    );
-  }, 0);
-};
-
 export default class AddressService {
-  constructor() {}
+  constructor() {
+    this.DistrictEntry = DistrictEntry;
+    this.LockedBalanceEvent = LockedBalanceEvent;
+  }
 
   static async lockedMANABalanceOf(address) {
     // get MANA locked to districts
-    const monthlyLockedBalancesToDistricts = await DistrictEntry.getMonthlyLockedBalanceByAddress(
-      address,
-      LAND_MANA_COST
-    ).then(balances => fillByMonth(balances));
+    const monthlyLockedBalancesToDistricts = await this.DistrictEntry
+      .getMonthlyLockedBalanceByAddress(address, LAND_MANA_COST)
+      .then(balances => fillByMonth(balances));
 
     // get total MANA locked to terraform
-    const monthlyLockedBalancesTotal = await LockedBalanceEvent.getMonthlyLockedBalanceByAddress(
-      address
-    ).then(balances => fillByMonth(balances));
+    const monthlyLockedBalancesTotal = await this.LockedBalanceEvent
+      .getMonthlyLockedBalanceByAddress(address)
+      .then(balances => fillByMonth(balances));
 
     // adjust MANA balances to bonuses
     const beforeNovBalanceToAuction = calculateTotalForMonths(
@@ -75,4 +45,31 @@ export default class AddressService {
       totalLockedToDistricts
     );
   }
+}
+
+function fillByMonth(items) {
+  const months = new Array(12).fill(0);
+  const groups = months.reduce(
+    (obj, _, index) => Object.assign(obj, { [index + 1]: 0 }),
+    {}
+  );
+
+  for (let { month, mana } of items) {
+    groups[month] = parseInt(mana, 10);
+  }
+  return groups;
+}
+
+function calculateTotalForMonths(
+  monthlyLockedBalancesToDistricts,
+  monthlyLockedBalancesTotal,
+  months
+) {
+  return months.reduce((total, index) => {
+    return (
+      total +
+      monthlyLockedBalancesTotal[index] -
+      monthlyLockedBalancesToDistricts[index]
+    );
+  }, 0);
 }
