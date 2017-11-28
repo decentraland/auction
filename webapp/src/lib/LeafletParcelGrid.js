e5d4a572e0e5afa33d09c4f0b8cdc11512ddcc37
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-11-28 13:01:53 +0100

    Consistent coding style

--- webapp/src/lib/LeafletParcelGrid.js
+++ webapp/src/lib/LeafletParcelGrid.js
@@ -1,7 +1,5 @@
 import L from 'leaflet'
 
-import { buildCoordinate } from './util'
-
 const LeafletParcelGrid = L.FeatureGroup.extend({
   include: L.Mixin.Events,
   options: {
@@ -19,12 +17,12 @@ const LeafletParcelGrid = L.FeatureGroup.extend({
     }
   },
 
-  initialize: function(options) {
+  initialize(options) {
     L.Util.setOptions(this, options)
     L.FeatureGroup.prototype.initialize.call(this, [], options)
   },
 
-  onAdd: function(map) {
+  onAdd(map) {
     L.FeatureGroup.prototype.onAdd.call(this, map)
     this.map = map
     this.cells = []
@@ -35,7 +33,7 @@ const LeafletParcelGrid = L.FeatureGroup.extend({
     map.on('resize', this.resizeHandler, this)
   },
 
-  onRemove: function(map) {
+  onRemove(map) {
     L.FeatureGroup.prototype.onRemove.call(this, map)
 
     map.off('moveend', this.moveHandler, this)
@@ -43,40 +41,61 @@ const LeafletParcelGrid = L.FeatureGroup.extend({
     map.off('resize', this.resizeHandler, this)
   },
 
-  clearLayer: function(e) {
+  clearLayer() {
     this.cells = []
   },
 
-  moveHandler: function(e) {
-    this.renderCells(e.target.getBounds())
+  moveHandler(event) {
+    this.renderCells(event.target.getBounds())
   },
 
-  zoomHandler: function(e) {
+  zoomHandler(event) {
     this.clearLayers()
-    this.renderCells(e.target.getBounds())
+    this.renderCells(event.target.getBounds())
   },
 
-  renderCells: function(bounds) {
-    let cells = this.cellsInBounds(bounds)
+  resizeHandler() {
+    this.setupSize()
+  },
+
+  setupGrid(bounds) {
+    this.origin = this.map.project(bounds.getNorthWest())
+    this.cellSize = this.options.cellSize
+
+    this.setupSize()
+
+    this.loadedCells = {}
+    this.loadedCoordinates = {}
+
+    this.clearLayers()
+    this.renderCells(bounds)
+  },
+
+  setupSize() {
+    this.rows = Math.ceil(this.map.getSize().x / this.cellSize)
+    this.cols = Math.ceil(this.map.getSize().y / this.cellSize)
+  },
+
+  renderCells(bounds) {
+    const cells = this.getCellsInBounds(bounds)
     this.fire('newcells', cells)
 
     for (let index = cells.length - 1; index >= 0; index--) {
-      this.loadCell(cells[index], index)
+      this.loadCell(cells[index], this.options.delayFactor * index)
     }
   },
 
-  loadCell(cell, index) {
+  loadCell(cell, renderDelay) {
     const { className, style, coordinates } = this.options.getTileAttributes(
       cell.center
     )
 
     if (this.loadedCells[cell.id] !== className) {
       const rectStyles = Object.assign({ className }, this.options.style, style)
-      const delay = this.options.delayFactor * index
 
       setTimeout(
         () => this.addLayer(L.rectangle(cell.bounds, rectStyles)),
-        delay
+        renderDelay
       )
 
       if (coordinates) {
@@ -96,65 +115,45 @@ const LeafletParcelGrid = L.FeatureGroup.extend({
           html: coordinates
         })
       })
+
       setTimeout(() => this.addLayer(marker))
+
       this.loadedCoordinates[cell.id] = true
     }
   },
 
-  resizeHandler: function(e) {
-    this.setupSize()
-  },
-
-  setupSize: function() {
-    this.rows = Math.ceil(this.map.getSize().x / this.cellSize)
-    this.cols = Math.ceil(this.map.getSize().y / this.cellSize)
-  },
-
-  setupGrid: function(bounds) {
-    this.origin = this.map.project(bounds.getNorthWest())
-    this.cellSize = this.options.cellSize
-    this.setupSize()
-    this.loadedCells = {}
-    this.loadedCoordinates = {}
-    this.clearLayers()
-    this.renderCells(bounds)
-  },
-
-  cellPoint: function(row, col) {
-    let x = this.origin.x + row * this.cellSize
-    let y = this.origin.y + col * this.cellSize
+  getCellPoint(row, col) {
+    const x = this.origin.x + row * this.cellSize
+    const y = this.origin.y + col * this.cellSize
     return new L.Point(x, y)
   },
 
-  cellExtent: function(row, col) {
-    let swPoint = this.cellPoint(row, col)
-    let nePoint = this.cellPoint(row - 1, col - 1)
-    let sw = this.map.unproject(swPoint)
-    let ne = this.map.unproject(nePoint)
+  getCellExtent(row, col) {
+    const swPoint = this.getCellPoint(row, col)
+    const nePoint = this.getCellPoint(row - 1, col - 1)
+    const sw = this.map.unproject(swPoint)
+    const ne = this.map.unproject(nePoint)
     return new L.LatLngBounds(ne, sw)
   },
 
-  cellsInBounds: function(bounds) {
-    let offset = this.map.project(bounds.getNorthWest())
-    let center = bounds.getCenter()
-    let offsetX = this.origin.x - offset.x
-    let offsetY = this.origin.y - offset.y
-    let offsetRows = Math.round(offsetX / this.cellSize)
-    let offsetCols = Math.round(offsetY / this.cellSize)
+  getCellsInBounds(bounds) {
+    const offset = this.getBoundsOffset(bounds)
+    const mainCenter = bounds.getCenter()
 
-    let cells = []
+    const cells = []
 
     for (let i = 0; i <= this.rows; i++) {
       for (let j = 0; j <= this.cols; j++) {
-        let row = i - offsetRows
-        let col = j - offsetCols
-        let cellBounds = this.cellExtent(row, col)
+        const row = i - offset.rows
+        const col = j - offset.cols
+        const cellBounds = this.getCellExtent(row, col)
+        const cellCenter = cellBounds.getCenter()
 
         cells.push({
-          id: buildCoordinate(row, col),
+          id: row + ':' + col,
           bounds: cellBounds,
-          center: cellBounds.getCenter(),
-          distance: cellBounds.getCenter().distanceTo(center)
+          center: cellCenter,
+          distance: cellCenter.distanceTo(mainCenter)
         })
       }
     }
@@ -162,6 +161,17 @@ const LeafletParcelGrid = L.FeatureGroup.extend({
     cells.sort((a, b) => a.distance - b.distance)
 
     return cells
+  },
+
+  getBoundsOffset(bounds) {
+    const offset = this.map.project(bounds.getNorthWest())
+    const offsetX = this.origin.x - offset.x
+    const offsetY = this.origin.y - offset.y
+
+    return {
+      rows: Math.round(offsetX / this.cellSize),
+      cols: Math.round(offsetY / this.cellSize)
+    }
   }
 })
 
