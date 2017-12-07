ce376468e220e06e90dfdb84efc98598dc809884
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-12-07 16:05:20 +0100

    New text and styling for error pages

--- webapp/src/components/WalletErrorPage.js
+++ webapp/src/components/WalletErrorPage.js
@@ -1,33 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import locations from '../locations'
-
 import ErrorPage from './ErrorPage'
 
 export default function WalletErrorPage() {
   return (
     <ErrorPage>
-      <h2>Wallet error</h2>
-      <p>We could not retrieve your account&#39;s information.</p>
-      <p>You may not be using a dApp compatible browser.</p>
-      <p>Is your account connected and unlocked?</p>
-      <br />
-      <p>
-        You can use&nbsp;
-        <Link to="https://metamask.io/" target="_blank">
-          Metamask
-        </Link>
-        &nbsp;or&nbsp;
-        <Link to="https://github.com/ethereum/mist" target="_blank">
-          Mist
-        </Link>.
-      </p>
-      <br />
-      <p>
-        Once you&#39;re done you can try again&nbsp;
-        <Link to={locations.root}>here</Link>.
-      </p>
+      <h2>
+        Uh-oh. <br />
+        We couldn’t retrieve your wallet information.
+      </h2>
+      <div className="error-message">
+        <p>
+          Please make sure your&nbsp;
+          <Link to="https://metamask.io" target="_blank">
+            Metamask
+          </Link>
+          &nbsp; or&nbsp;
+          <Link to="https://github.com/ethereum/mist" target="_blank">
+            Mist
+          </Link>
+          &nbsp;account is connected and unlocked, then refresh the page.
+        </p>
+        <br />
+        <p>
+          If you’ve just installed Metamask or Mist, restarting your browser
+          usually fixes the problem.
+        </p>
+        <br />
+        <p>
+          Confused about what&#39;s going on? Check out the&nbsp;
+          <Link to="https://wiki.decentraland.org/" target="_blank">
+            wiki
+          </Link>&nbsp; for answers.
+        </p>
+      </div>
     </ErrorPage>
   )
 }
