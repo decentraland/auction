97fdfd6e52e6a1b4f593a22d598f0691e70f8b43
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-12-12 12:07:23 +0100

    Don't show the balance on the expanded sidebar dashboard

--- webapp/src/components/SidebarDashboard.js
+++ webapp/src/components/SidebarDashboard.js
@@ -6,27 +6,27 @@ import './SidebarDashboard.css'
 export default function SidebarDashboard({ dashboard }) {
   return (
     <ul className="SidebarDashboard">
-      <li>
+      <li className="balance">
         <div className="dashboard-heading">BALANCE</div>
         <div className="dashboard-value">{dashboard.balance}</div>
       </li>
-      <li>
+      <li className="bids">
         <div className="dashboard-heading">BIDS</div>
         <div className="dashboard-value">{dashboard.bids}</div>
       </li>
-      <li>
+      <li className="winning">
         <div className="dashboard-heading">WINNING</div>
         <div className="dashboard-value">{dashboard.winning}</div>
       </li>
-      <li>
+      <li className="losing">
         <div className="dashboard-heading">LOSING</div>
         <div className="dashboard-value">{dashboard.losing}</div>
       </li>
-      <li>
+      <li className="won">
         <div className="dashboard-heading">WON</div>
         <div className="dashboard-value">{dashboard.won}</div>
       </li>
-      <li>
+      <li className="lost">
         <div className="dashboard-heading">LOST</div>
         <div className="dashboard-value">{dashboard.lost}</div>
       </li>
