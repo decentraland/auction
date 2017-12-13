2d054c70788419d6ef37929928aedb548ba9747e
Esteban Ordano <esteban@decentraland.org>
2017-12-13 10:33:01 -0300

    Minimap movement

--- webapp/src/containers/MinimapContainer.js
+++ webapp/src/containers/MinimapContainer.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
+import { changeLocation } from '../actions'
+import locations from '../locations'
 import { selectors } from '../reducers'
 
 import Minimap from '../components/Minimap.js'
@@ -12,15 +14,19 @@ class MinimapContainer extends React.Component {
       minX: PropTypes.number,
       minY: PropTypes.number,
       maxX: PropTypes.number,
-      maxY: PropTypes.number
+      maxY: PropTypes.number,
     })
   }
 
+  update = (x, y) => this.props.changeLocation(locations.parcelDetail(x, y))
+
   render() {
-    return <Minimap {...this.props.range} />
+    return <Minimap {...this.props.range} update={this.update.bind(this)} />
   }
 }
 
 export default connect(state => ({
   range: selectors.getRange(state)
-}))(MinimapContainer)
+}), {
+  changeLocation
+})(MinimapContainer)
