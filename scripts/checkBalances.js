17717492cfb263b79700ca20b0a10d71f34942de
Ariel Barmat <abarmat@gmail.com>
2017-12-26 03:50:59 -0300

    Validate also addresses with initial zero balance

--- scripts/checkBalances.js
+++ scripts/checkBalances.js
@@ -29,21 +29,21 @@ async function checkBalances() {
     // get initial balance
     const lockedMANA = await addressService.lockedMANABalanceOf(address)
     const initialBalance = lockedMANA.totalLockedMANA - lockedMANA.totalLandMANA
-    if (!initialBalance) {
-      continue
-    }
 
+    // get current balance
     const addressState = await AddressState.findByAddress(address)
     if (addressState) {
       total += 1
       const currentBalance = addressState.balance
+
+      // get bids
       const winningParcels = await ParcelState.findByAddress(address)
       const bidding = winningParcels.reduce(
         (sum, item) => sum + parseInt(item.amount, 10),
         0
       )
 
-      // error
+      // balance mismatch
       if (initialBalance - bidding != currentBalance) {
         mismatch += 1
         log.info(
@@ -51,6 +51,8 @@ async function checkBalances() {
             bidding}\twinning:${winningParcels.length}`
         )
       }
+    } else {
+      log.warn(`(${address}) address state not found!`)
     }
   }
   log.info(`Found ${mismatch} discrepancies out of ${total} addresses`)
