929fdbe31143c62086bb0397b89329bc3b12c738
Ariel Barmat <abarmat@gmail.com>
2017-12-26 03:48:20 -0300

    Lint. Remove unused code. Insert 0 balance addresses. Get locked addresses from query.

--- scripts/insertMissing.js
+++ scripts/insertMissing.js
@@ -1,21 +1,15 @@
 #!/usr/bin/env babel-node
 
-import fs from 'fs'
-import { execSync } from 'child_process'
 import { env, Log } from 'decentraland-commons'
 import db from '../src/lib/db'
 import {
   AddressState,
   ParcelState,
-  DistrictEntry,
-  Project,
   LockedBalanceEvent
 } from '../src/lib/models'
-import { AddressService, ParcelStateService } from '../src/lib/services'
+import { AddressService } from '../src/lib/services'
 
 const log = new Log('init')
-const parcelStateService = new ParcelStateService()
-const parcelsDescription = getPracelsDescriptions()
 
 env.load()
 
@@ -25,44 +19,31 @@ async function initializeDatabase() {
 }
 
 async function insertMissingAddresses() {
-
-  const events = await LockedBalanceEvent.find()
-  const addressesMap = {}
-  for (const event of events) {
-    addressesMap[event.address] = 1
-  }
-  const addresses = Object.keys(addressesMap)
+  // get all addresses with events
+  const addresses = await LockedBalanceEvent.getLockedAddresses()
   const addressService = new AddressService()
 
-  const cleared = fs
-    .readFileSync('./clearAddress.txt', 'utf8')
-    .split('\n')
-    .map(address => address.toLowerCase())
-    .filter(e => !!e)
-
   for (const address of addresses) {
+    // get initial balance
     const lockedMANA = await addressService.lockedMANABalanceOf(address)
-    const balance = lockedMANA.totalLockedMANA - lockedMANA.totalLandMANA
+    const initialBalance = lockedMANA.totalLockedMANA - lockedMANA.totalLandMANA
 
     const addressState = await AddressState.findByAddress(address)
     if (addressState) {
-      if (balance > 0 && addressState.balance != balance) {
+      if (initialBalance > 0 && addressState.balance != initialBalance) {
         const winningParcels = await ParcelState.findByAddress(address)
         if (!winningParcels.length) {
-          log.info(`[${address}] Updating balance\toriginal ${balance}\tdb had ${addressState.balance} balance`)
-          await AddressState.update({ balance }, { address })
+          log.info(
+            `[${address}] Updating balance\toriginal ${initialBalance}\tdb had ${addressState.balance} balance`
+          )
+          await AddressState.update({ balance: initialBalance }, { address })
         }
       }
-    } else if (balance > 0) {
-      log.info(`[${address}] Inserting balance(${balance})`)
-      await AddressState.insert({ address, balance })
+    } else {
+      log.info(`[${address}] Inserting balance(${initialBalance})`)
+      await AddressState.insert({ address, balance: initialBalance })
     }
   }
-}
-
-function getPracelsDescriptions() {
-  let parcelsDescription = fs.readFileSync('./parcelsDescription.json', 'utf8')
-  return JSON.parse(parcelsDescription)
 }
 
 db
